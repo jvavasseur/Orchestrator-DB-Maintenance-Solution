4802d3f6aa73d35906a4a0cef7acf05740bcc38d
Julien Vavasseur <sign.github@glop.info>
2019-11-26 17:25:01 +0100

    File creation

---
+++ SQLServer-Cleanup/Schema_Maintenance.sql
@@ -0,0 +1,10 @@
+SET NOCOUNT ON
+GO
+
+IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'Maintenance')
+BEGIN 
+	PRINT ' + Create Schema [Maintenance]';
+	EXEC sp_executesql N'CREATE SCHEMA [Maintenance]';
+END
+ELSE PRINT ' = Schema already exists: [Maintenance]';
+GO
