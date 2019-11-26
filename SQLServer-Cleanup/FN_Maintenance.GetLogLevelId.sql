579809bae08d62b9fdfa1e4b33840e8af7043c53
Julien Vavasseur <sign.github@glop.info>
2019-11-26 17:25:35 +0100

    Remove schema creation Add return comment

--- SQLServer-Cleanup/FN_Maintenance.GetLogLevelId.sql
+++ SQLServer-Cleanup/FN_Maintenance.GetLogLevelId.sql
@@ -1,20 +1,4 @@
---    DROP FUNCTION [Maintenance].[GetLogLevelId]
 SET NOCOUNT ON
-GO
-
-PRINT 'CREATE SCHEMA';
-GO
-
-IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'Maintenance')
-BEGIN 
-	PRINT ' + Create Schema [Maintenance]';
-	EXEC sp_executesql N'CREATE SCHEMA [Maintenance]';
-END
-ELSE PRINT ' = Schema already exists: [Maintenance]';
-GO
-PRINT ''
-
-PRINT 'CREATE FUNCTION';
 GO
 
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Maintenance].[GetLogLevelId]') AND type = N'FN')
@@ -26,8 +10,8 @@ ELSE PRINT ' = Function [Maintenance].[GetLogLevelId] already exists';
 GO
 
 PRINT ' ~ Update Function [Maintenance].[GetLogLevelId]';
-
 GO
+
 ----------------------------------------------------------------------------------------------------
 -- Returns the Id of the input level Id or Name or NULL if the value doesn't exists
 --   Intput @level = text value with evel numeric id or level name
@@ -42,6 +26,7 @@ BEGIN
     FROM [Maintenance].[LogLevels]()
     WHERE Id = TRY_PARSE(@level AS int) OR Level = LTRIM(RTRIM(@level));
 
+    -- Return level Id when @level is matched with Id or Name and NULL otherwise
     RETURN(@Id);
 END
 GO
