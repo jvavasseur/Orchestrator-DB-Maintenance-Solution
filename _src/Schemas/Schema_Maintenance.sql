5850c019c5f78a21fbe570239d696d96f287c3d3
Julien <sign.github@glop.info>
2021-03-11 16:24:39 +0100

    Update Schema_Maintenance.sql

--- _src/Schemas/Schema_Maintenance.sql
+++ _src/Schemas/Schema_Maintenance.sql
@@ -1,6 +1,12 @@
 SET NOCOUNT ON;
 GO
 
+----------------------------------------------------------------------------------------------------
+-- ### [Object]: Schema [Maintenance]
+-- ### [Version]: 2020-10-01 00:00:00                                                         
+-- ### [Hash]: XxXxXxX
+-- ### [Docs]: https://XxXxXxX
+----------------------------------------------------------------------------------------------------
 IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'Maintenance')
 BEGIN 
 	PRINT ' + Create Schema [Maintenance]';
